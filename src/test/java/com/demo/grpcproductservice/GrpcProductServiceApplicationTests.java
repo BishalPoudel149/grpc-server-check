ea5bc36341edf98744e7a83ed828badc5f2fb20f
Bishal Poudel <bishal.poudel@sap.com>
2024-12-25 13:04:13 +0530

    first commit

---
+++ src/test/java/com/demo/grpcproductservice/GrpcProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.grpcproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
